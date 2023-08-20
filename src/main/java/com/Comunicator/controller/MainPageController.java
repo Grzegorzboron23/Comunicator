77b7ffdd594cacdb6b06b5768a16701d3c8507f8
Grzegorz Boroń <grzegorzboron23@gmail.com>
2023-08-20 08:22:25 +0200

    add Dynamicly generated search field

--- src/main/java/com/Comunicator/controller/MainPageController.java
+++ src/main/java/com/Comunicator/controller/MainPageController.java
@@ -7,10 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
@@ -49,12 +46,14 @@ public class MainPageController {
 
     @PostMapping("/processData")
     @ResponseBody
-    public String processData(@RequestBody String inputValue) {
+    public List<User> processData(@RequestParam String inputValue ) {
         System.out.println("jestem w kontrolerze springa " + inputValue);
 
-
-        String processedValue = "Przetworzona wartość: " + inputValue;
-        return processedValue;
+        if(inputValue != null) {
+            List<User> userList = userRepository.findByUsernameContaining(inputValue);
+            return userList;
+        }
+        return null;
     }
 
 }
--- src/main/resources/static/js/mainPage.js
+++ src/main/resources/static/js/mainPage.js
@@ -1,3 +1,7 @@
+
+    let chosenUserId=null;
+
+
  function sendMessage() {
             const message = document.getElementById("messageInput").value;
             const fromLogin = document.getElementById("userId").value;
@@ -7,8 +11,9 @@
             const socket = new SockJS('/ws');
             const stompClient = Stomp.over(socket);
 
+            console.log("Zaraz wysle wiadomosc do usera o id= " + chosenUserId);
             stompClient.connect({}, function(frame) {
-                stompClient.send("/app/chat/6", {}, JSON.stringify({
+                stompClient.send("/app/chat/" + chosenUserId, {}, JSON.stringify({
                     message: message,
                     fromLogin: fromLogin
                 }));
@@ -17,11 +22,54 @@
 
 
 
-  $("#searchInput").on("input", function() {
-    var inputValue = $(this).val();
+   $(document).ready(function() {
+      $("#searchInput").on("input", function() {
+        var inputValue = $(this).val();
+      console.log("Wpisano literę: " + inputValue);
     console.log("Wpisano literę: " + inputValue);
 
 
-    //TODO AJAX findUsers dynamiccly like lookup
+       $.ajax({
+                  type: "POST",
+                  url: "/processData",
+                  data: { inputValue: inputValue },
+                  success: function(response) {
+                  console.log("Jestem tutaj dziala");
+                      var selectHtml = '<select id="userSelect">';
 
+                      response.forEach(function(user) {
+                          selectHtml += '<option value="' + user.id + '">' + user.name + '</option>';
+                      });
+
+                      selectHtml += '</select>';
+
+                      $("#selectContainer").html(selectHtml);
+
+                      $("#userSelect").on("change", function() {
+                          var selectedUserId = $(this).val();
+
+                          if (selectedUserId !== "") {
+                              chosenUserId = selectedUserId;
+                              showInputAndButton();
+                              console.log("Wybrano użytkownika o ID: " + selectedUserId);
+                          }
+                      });
+                  },
+                  error: function (xhr, status, error) {
+                       console.error("Error during AJAX request. Status: " + status + ", Error: " + error);
+                       console.log("XHR object:", xhr);
+                  },
+                  complete: function () {
+
+                  }
+              });
+
+
+            });
   });
+   function showInputAndButton() {
+       document.getElementById("messageInput").hidden = false;
+       document.getElementById("sendButton").hidden = false;
+   }
+
+
